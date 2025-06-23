64eb7e4dce784edf9622f4306e34f644da6558db
Joaquin Abarzua <joaquin.abarzua@mail.udp.cl>
2025-06-22 22:42:41 -0400

    falta todo lo que es jugar o lo que ocurre durante el juego

--- src/Main.java
+++ src/Main.java
@@ -36,7 +36,11 @@ class Player {
         // también hay que ver la forma de cortar los decimales, para
         // que el porcentaje quede como entero (13% pero no 13.5%)
     }
+
+    public Integer getWins(){ return this.wins; }
 }
+
+
 
 //------------------------Clase Scoreboard--------------------------
 
@@ -72,8 +76,14 @@ class Scoreboard {
     public void registerPlayer (String playerName){
         if(!players.containsKey(playerName)) {
             Player p = new Player(playerName);
-            players.put(player, p);
-            addToWinTree(p);
+            players.put(playerName, p);
+
+            //si aún no se ha creado la lista con 0 ganadas
+            if (!winTree.containsKey(0)) {
+                winTree.put(0, new ArrayList<>());
+            } //crea la lista
+
+            winTree.get(0).add(playerName);
         }
     }
 
@@ -83,9 +93,13 @@ class Scoreboard {
             List<Player> result = new Arraylist<>();
 
             for(Integer ganadas : winTree.keySet()) { //.keySet() retorna un Set (conjunto) de llaves iterables (para recorrerlas)
-                if (ganadas >= lo && ganadas <= hi) {
+                //"ganadas" es un Integer que recorre todas las llaves del mapa (que también son Integer)
+                //entonces "ganadas" irá tomando el valor de cada llave
+                if (ganadas >= lo && ganadas <= hi) { //revisa si la llave está dentro del rango buscado
                     List<String> jugadores = winTree.get(ganadas);
-                    for (String nombreJugador : jugadores) {
+
+                    //"nombreJugador" es un String que recorre cada elemento de la lista que esté en la llave del mapa
+                    for (String nombreJugador : jugadores) { //y los va agregando a la lista "result"
                         result.add(players.get(nombreJugador));
                     }
                 }
@@ -97,13 +111,15 @@ class Scoreboard {
 
 
     public Player[] winSuccesor (int wins) {
-        Interger higher = winTree.higherKey(wins);
+        Interger higher = winTree.higherKey(wins); //.higherKey() retorna la llave más pequeña que sea estrictamente mayor
+        // que la llave entregada, si no existe, retorna null
+
         if(higher == null){
-            return new Player[0]; // si no hay retorna nada 
+            return new Player[0]; // si no hay una llave mayor, retorna nada
         }
-        list<Player> result = new Arraylist<>();
-        for ( string name : winTree.get(higher)){
-        result.add(players.get(name));
+        List<Player> result = new Arraylist<>();
+        for ( String nombre : winTree.get(higher)){
+            result.add(players.get(nombre));
         }
         return result.toArray(new Player[0]);
     }
@@ -118,37 +134,57 @@ class ConnectFour {
     char currentSymbol;
 
     public ConnectFour() {
-        grid = new char[7][6];
-
-        //llenar todas las filas y columnas con "espacio"
-        for (int i = 0; i < 7; i++) {
+        grid = new char[7][6]; //habría que cambiarlo, el juego original son 6 filas y 7 columnas (aquí son 7 filas 7 6 columnas)
+        for (int i = 0; i < 7; i++) {//llenar todas las filas y columnas con "espacio"
             for (int j = 0; j < 6; j++) {
                 grid[i][j] = ' ';
             }
         }
-
         this.currentSymbol = 'X';
     }
 
-    public boolean makeMove(int z) {
+
+    public boolean makeMove(int z) {    //z es la columna escogida
+        if (z < 0 || z > 5 || grid[6][z] != ' ') { return false; }//por ahora dejo el método revisando que la columna escogida sean dentro
+        // de lass 6 disponibles (en vez de 7 que sería lo original)
+        //y también revisa que la fila de más arriba no esté ocupada
+
+        if (grid[0][z] == ' ') {    //reviso que la fila de más abajo esté libre para hacer el movimiento de una
+            grid[0][z] = currentSymbol;
+            if (currentSymbol == 'X') { currentSymbol = 'O'; }
+            else if (currentSymbol == 'O') { currentSymbol = 'X'; }
+            return true;
+        }
+
+        int i = 6;  //comienzo a revisar desde la fila de más arriba
+        while (grid[i-1][z] == ' ') {   //reviso que la de abajo esté libre
+            i--;    //sí está libre, me voy a la de abajo
+        }
+
+        if (grid[i][z] == ' ') {    //reviso que esté vacía para hacer el movimiento y retornar true
+            grid[i][z] = currentSymbol;
+            if (currentSymbol == 'X') { currentSymbol = 'O'; }
+            else if (currentSymbol == 'O') { currentSymbol = 'X'; }
+            return true;
+        }
+        return false;
     }
 
-    public void isGameOver() {
-    } //por ahora void, hay que definir cómo se
-    // hará el método
+    public void isGameOver() { } //por ahora void, ni si quiera sé cómo hacer este método
 
 }
 
 //-------------------------Clase Game------------------------
+
 class Game {
     private String status;
     private String winnerPlayerName;
     private String playerNameA;
     private String playerNameB;
-    private ConnectFour connectFour;
+    private ConnectFour ConnectFour;
 
     public Game (String playerNameA, String playerNameB) {
-        connectFour = new ConnectFour();
+        ConnectFour = new ConnectFour();
         this.playerNameA = playerNameA;
         this.playerNameB = playerNameB;
         this.status = "IN_PROGRESS";
