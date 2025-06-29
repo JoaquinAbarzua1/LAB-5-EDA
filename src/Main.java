4d3e8842e2261bb8fab27a78f95de0bb772b5331
Joaquin Abarzua <joaquin.abarzua@mail.udp.cl>
2025-06-29 03:03:47 -0400

    creo que está listo

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ class Player {
     private int wins;
     private int draws ;
     private int losses;
-    private int jugadas; //
+    private int jugadas; //para tener eltiro el total de jugadas
 
     public Player (String playerName) {
         this.playerName = playerName;
@@ -38,6 +38,9 @@ class Player {
     }
 
     public Integer getWins(){ return this.wins; }
+    public Integer getDraws(){ return this.draws; }
+    public Integer getLosses(){ return this.losses; }
+    public Integer getTotal() {return this.jugadas;}
 }
 
 
@@ -55,7 +58,6 @@ class Scoreboard {
         winTree = new TreeMap<>();
         players = new HashMap<>();
         playedGames = 0;
-
     }
 
     public void addGameResult (String winnerPlayerName, String looserPlayerName, boolean draw){
@@ -65,9 +67,18 @@ class Scoreboard {
         }
         else{
             players.get(looserPlayerName).addLoss();
-            players.get(winnerPlayerName).addWin();
-            //falta actualizar winTree, pero hay que ver cómo sacar al jugador, actualizarle sus ganadas
-            // y volver a insertarlo donde corresponda
+
+            List<String> listaActual = winTree.get(players.get(winnerPlayerName).getWins()); //lista donde está actualmente el ganador
+            listaActual.remove(players.get(winnerPlayerName)); //lo saco de la lista
+
+
+            players.get(winnerPlayerName).addWin(); //le agrego la victoria
+            List<String> listaNueva = winTree.get(players.get(winnerPlayerName).getWins()); //voy a la lista que corresponde a la nueva cantidad de victorias
+            if (listaNueva == null){ //reviso si la lista existe
+                listaNueva = new ArrayList<>();
+                winTree.put(players.get(winnerPlayerName).getWins(), listaNueva);
+            }
+            listaNueva.add(winnerPlayerName);
         }
         playedGames++;
 
@@ -87,7 +98,10 @@ class Scoreboard {
         }
     }
 
- //check player ingresado en el register
+    public boolean checkPlayer (String playerName) {
+        return players.containsKey(playerName);
+    }
+    //check player ingresado en el register (creo que igual hay que agregarlo, por más innecesario que sea, el ayudante me dijo que lo ponga igual)
 
     public Player[] winRange (int lo, int hi){
             List<Player> result = new Arraylist<>();
@@ -111,19 +125,22 @@ class Scoreboard {
 
 
     public Player[] winSuccesor (int wins) {
-        Interger higher = winTree.higherKey(wins); //.higherKey() retorna la llave más pequeña que sea estrictamente mayor
+        Integer higher = winTree.higherKey(wins); //.higherKey() retorna la llave más pequeña que sea estrictamente mayor
         // que la llave entregada, si no existe, retorna null
 
         if(higher == null){
             return new Player[0]; // si no hay una llave mayor, retorna nada
         }
-        List<Player> result = new Arraylist<>();
+        List<Player> result = new ArrayList<>();
         for ( String nombre : winTree.get(higher)){
             result.add(players.get(nombre));
         }
         return result.toArray(new Player[0]);
     }
 
+    public Player getPlayer(String playerName) {
+        return players.get(playerName);
+    }
 
 }
 
@@ -134,43 +151,65 @@ class ConnectFour {
     char currentSymbol;
 
     public ConnectFour() {
-        grid = new char[7][6]; //habría que cambiarlo, el juego original son 6 filas y 7 columnas (aquí son 7 filas 7 6 columnas)
-        for (int i = 0; i < 7; i++) {//llenar todas las filas y columnas con "espacio"
-            for (int j = 0; j < 6; j++) {
-                grid[i][j] = ' ';
+        grid = new char[7][6];
+        for (int fila = 0; fila < 7; fila++) {//llenar todas las filas y columnas con "espacio"
+            for (int col = 0; col < 6; col++) {
+                grid[fila][col] = ' ';
             }
         }
         this.currentSymbol = 'X';
     }
 
+    public char getCurrentSymbol() { return currentSymbol; }
 
     public boolean makeMove(int z) {    //z es la columna escogida
-        if (z < 0 || z > 5 || grid[6][z] != ' ') { return false; }//por ahora dejo el método revisando que la columna escogida sean dentro
-        // de lass 6 disponibles (en vez de 7 que sería lo original)
+        if (z < 0 || z > 5) {
+            System.out.println("Movimiento no valido, intentelo de nuevo...");
+            return false; }//revisa que la columna escogida sea dentro
+        // de las 6 disponibles
         //y también revisa que la fila de más arriba no esté ocupada
+        if (grid[0][z] != ' '){
+            System.out.println("Columna completa, elija otra");
+            return false;
+        }
 
-        if (grid[0][z] == ' ') {    //reviso que la fila de más abajo esté libre para hacer el movimiento de una
-            grid[0][z] = currentSymbol;
+        if (grid[6][z] == ' ') {    //reviso que la fila de más abajo esté libre para hacer el movimiento altiro
+            grid[6][z] = currentSymbol;
             if (currentSymbol == 'X') { currentSymbol = 'O'; }
             else if (currentSymbol == 'O') { currentSymbol = 'X'; }
             return true;
         }
 
-        int i = 6;  //comienzo a revisar desde la fila de más arriba
-        while (grid[i-1][z] == ' ') {   //reviso que la de abajo esté libre
-            i--;    //sí está libre, me voy a la de abajo
+        int fila = 0;  //comienzo a revisar desde la fila de más arriba
+        while ((grid[fila+1][z] == ' ') && (fila < 5)) {   //reviso que la de abajo esté libre
+            fila++;    //sí está libre, me voy a la de abajo
         }
 
-        if (grid[i][z] == ' ') {    //reviso que esté vacía para hacer el movimiento y retornar true
-            grid[i][z] = currentSymbol;
+        if (grid[fila][z] == ' ') {    //reviso que esté vacía para hacer el movimiento y retornar true
+            grid[fila][z] = currentSymbol;
             if (currentSymbol == 'X') { currentSymbol = 'O'; }
             else if (currentSymbol == 'O') { currentSymbol = 'X'; }
             return true;
         }
+
         return false;
     }
 
     public void isGameOver() { } //por ahora void, ni si quiera sé cómo hacer este método
+
+
+    public void imprimirTablero() {
+        for (char [] fila : grid) {//imprimo toda la fila de una
+            for (char c : fila) { //recorro cada fila
+                System.out.print("|" + c);
+            }
+            System.out.print("|");
+            System.out.println();
+        }
+        System.out.println(" _ _ _ _ _ _ ");
+        System.out.println(" 1 2 3 4 5 6 ");
+    }
+
 
 }
 
@@ -179,6 +218,8 @@ class ConnectFour {
 class Game {
     private String status;
     private String winnerPlayerName;
+    private String looserPlayerName; //
+    private boolean draw;
     private String playerNameA;
     private String playerNameB;
     private ConnectFour ConnectFour;
@@ -190,11 +231,61 @@ class Game {
         this.status = "IN_PROGRESS";
     }
 
-    public void play(){ //tremendo método, hay que analizar
+    public String getWinnerPlayerName() { return winnerPlayerName; }
+    public String getLooserPlayerName() { return looserPlayerName; }
+    public boolean getDraw() { return draw; }
+    public void setStatus(String status) { this.status = status; }
 
+    public String play(){
+        char resultado;
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        while (status.equals("IN_PROGRESS")) { //mientras el juego esté en progreso
+
+            if (ConnectFour.getCurrentSymbol() == 'X') {
+                System.out.println("Turno de: " + playerNameA);
+            } else if (ConnectFour.getCurrentSymbol() == 'O') {
+                System.out.println("Turno de: " + playerNameB);
+            }
+
+            ConnectFour.imprimirTablero();//imprime el tablero en cada ciclo
+
+
+            int columna;
+            do { //ciclo que primero ejecuta y luego revisa condición
+                System.out.println("Ingrese una columna [1-6]: ");
+                columna = scanner.nextInt();
+            } while (!ConnectFour.makeMove(columna - 1)); //mientras no se haga un movimiento válido, se repite
+
+            resultado = ConnectFour.isGameOver();
+            if(resultado != ' ') {
+                ConnectFour.imprimirTablero(); //cuando alguien ganaba no se imprimia la jugada ganadora
+                if (resultado == 'D'){
+                    status = "DRAW";
+                    draw = true;
+                    return "EMPATE";
+
+                }
+                else if (resultado == 'X'){
+                    status = "VICTORY";
+                    winnerPlayerName = playerNameA;
+                    looserPlayerName = playerNameB;
+                    draw = false;
+
+                }
+                else if (resultado == 'O'){
+                    status = "VICTORY";
+                    winnerPlayerName = playerNameB;
+                    looserPlayerName = playerNameA;
+                    draw = false;
+                }
+            }
+        }
+        return winnerPlayerName;
     }
 }
-
 
 
 
